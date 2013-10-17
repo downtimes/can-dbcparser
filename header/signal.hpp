be9a50cc6cd65ec449e2affc90afdc0fa0546818
Michael Auracher <michael.auracher@gmail.com>
2013-10-17 20:03:16 +0200

    First Comments in Code

--- header/signal.hpp
+++ header/signal.hpp
@@ -27,26 +27,47 @@ enum class Multiplexor {
 	MULTIPLEXOR
 };
 
+
+/**
+ * This class represents a Signal contained in a Message of a DBC-File.
+ * One can Query all the necessary information from this class to define
+ * a Signal
+ */
 class Signal {
 
 	typedef std::set<std::string> toList;
+	//The name of the Signal in the DBC-File
 	std::string name;
+	//The Byteorder of the Signal (@see: endianess)
 	ByteOrder order;
+	//The Startbit inside the Message of this Signal. Allowed values are 0-63
 	unsigned short startBit;
+	//The Length of the Signal. It can be anything between 1 and 64
 	unsigned short length;
+	//If the Data contained in the Signal is signed or unsigned Data
 	Sign sign;
+	//Depending on the information given above one can calculate the minimum of this Signal
 	double minimum;
+	//Depending on the inforamtion given above one can calculate the maximum of this Signal
 	double maximum;
+	//The Factor for calculating the physical value: phys = digits * factor + offset
 	double factor;
+	//The offset for calculating the physical value: phys = digits * factor + offset
 	double offset;
+	//String containing an associated unit.
 	std::string unit;
+	//Contains weather the Signal is Multiplexed and if it is, multiplexNum contains multiplex number
 	Multiplexor multiplexor;
+	//Contains the multiplex Number if the Signal is multiplexed
 	unsigned short multiplexNum;
+	//Contains to which Control Units in the CAN-Network the Signal shall be sent
 	toList to;
 
 public:
+	//Overload of operator>> to allow parsing from DBC Streams
 	friend std::istream& operator>>(std::istream& in, Signal& sig);
 
+	//Getter for all the Values contained in a Signal
 	std::string getName() const { return name; }
 	ByteOrder getByteOrder() const { return order; }
 	unsigned short getStartbit() const { return startBit; }
