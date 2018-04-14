5a2a854d428eeb1c63cd075f46dd633c0cb6dcfc
Coudoin, Bruno <Bruno.Coudoin@continental-corporation.com>
2018-04-14 23:08:24 +0200

    better default parsing of the given dbc in the main

--- main.cpp
+++ main.cpp
@@ -25,30 +25,26 @@ int main(int argc, char* argv[]) {
 
 	try {
 		DBCIterator dbc(argv[1]);
-		std::cout << dbc[0].getName() << "\n" << dbc[0].getDlc() << " " << dbc[0].getId() << " " << dbc[0].getFrom() << "\n";
-		for(auto sig : dbc[0]) {
-			std::cout << sig.getName() << " " << sig.getUnit() << std::endl;
-			std::cout << sig.getStartbit() << "," << sig.getLength() << std::endl;
-			std::cout << "(" << sig.getFactor() << ", " << sig.getOffset() << ")" << std::endl;
-			std::cout << "[" << sig.getMinimum() << "," << sig.getMaximum() << "]" << std::endl;
-			if (sig.getMultiplexor() == Multiplexor::MULTIPLEXED) {
-				std::cout << "#" << sig.getMultiplexedNumber() << "#" << std::endl;
-			} else if (sig.getMultiplexor() == Multiplexor::MULTIPLEXOR) {
-				std::cout << "+Multiplexor+" << std::endl;
-			}
-		};
-		std::cout << std::endl;
+        for(auto message : dbc) {
+            std::cout << message.getName() << " " << message.getId() << std::endl;
+            for(auto sig : message) {
+                std::cout << "Signal: " << sig.getName() << "  ";
+                std::cout << "To: ";
+                for (auto to : sig.getTo()) {
+                    std::cout << to << ", ";
+                }
+                std::cout << sig.getStartbit() << "," << sig.getLength() << std::endl;
+                std::cout << "(" << sig.getFactor() << ", " << sig.getOffset() << ")" << std::endl;
+                std::cout << "[" << sig.getMinimum() << "," << sig.getMaximum() << "]" << std::endl;
+                if (sig.getMultiplexor() == Multiplexor::MULTIPLEXED) {
+                    std::cout << "#" << sig.getMultiplexedNumber() << "#" << std::endl;
+                } else if (sig.getMultiplexor() == Multiplexor::MULTIPLEXOR) {
+                    std::cout << "+Multiplexor+" << std::endl;
+                }
+                std::cout << std::endl;
+            };
+        }
 
-		//
-		//	std::cout << messages[30].getName() << std::endl;
-		//	for(auto sig : messages[30]) {
-		//		std::cout << "Signal: " << sig.getName() << "  ";
-		//		std::cout << "To: ";
-		//		for (auto to : sig.getTo()) {
-		//			std::cout << to << ", ";
-		//		}
-		//		std::cout << std::endl;
-		//	};
 	} catch (std::invalid_argument& ex) {
 		std::cout << ex.what() << std::endl;
 	}
